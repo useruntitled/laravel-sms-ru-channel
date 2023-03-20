0e0640ee95fd144baaf01c20914ad090ce2f516c
S.Andriyanov <x2e8@yandex.ru>
2023-03-20 08:53:54 +0300

    fix lumen application

--- src/SmsRuProvider.php
+++ src/SmsRuProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace WebRegul\SmsRuChannel;
 
 use GuzzleHttp\Client;
-use Illuminate\Contracts\Foundation\Application;
+//use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use WebRegul\SmsRu\SmsRuApi;
@@ -15,9 +15,9 @@ final class SmsRuProvider extends ServiceProvider implements DeferrableProvider
 {
     public function register()
     {
-        $this->app->singleton(SmsRuApi::class, static function (Application $application) {
+        $this->app->singleton(SmsRuApi::class, static function ($app) {
             return new SmsRuApi(
-                new SmsRuConfig($application->make('config')['services.sms_ru']),
+                new SmsRuConfig($app->make('config')['services.sms_ru']),
                 new Client()
             );
         });
